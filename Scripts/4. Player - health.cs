b18008844dbd9d40e31122d60149b4eded2ed02e
munimik <71000737+munimik@users.noreply.github.com>
2021-04-28 16:01:03 +0530

    Create 4. Player - health.cs

---
+++ Scripts/4. Player - health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float speed;
+    public float Increment;
+
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Increment);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Increment);
+        }
+    }
+}
