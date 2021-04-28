9c133ab37e7c1c5397b84b5427428dd37cc45dd4
munimik <71000737+munimik@users.noreply.github.com>
2021-04-28 22:11:59 +0530

    Create 9.Player Effect.cs

---
+++ Scripts/9.Player Effect.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float speed;
+    public float Increment;
+
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+
+    public GameObject Effect;
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            Instantiate(Effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + Increment);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            Instantiate(Effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - Increment);
+        }
+    }
+}
